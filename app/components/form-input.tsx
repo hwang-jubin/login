f4b2c0e81f81bec2e41d01568e7176788bd537ea
황주빈 <hjb06288@gmail.com>
2024-11-06 16:10:46 +0900

    feat: form 의 형식에 따른 아이콘

--- app/components/form-input.tsx
+++ app/components/form-input.tsx
@@ -1,16 +1,41 @@
 "use client";
 
-import { EnvelopeIcon } from "@heroicons/react/24/solid";
+import { EnvelopeIcon, UserIcon, KeyIcon } from "@heroicons/react/24/solid";
+import { InputHTMLAttributes } from "react";
 
-export default function FormInput() {
+interface InputProps {
+  name: string;
+  errors?: string[];
+}
+
+type IconName = "email" | "username" | "password";
+
+// IconName의 type에 value는 JSX.Element
+const icon: Record<IconName, JSX.Element> = {
+  email: (
+    <EnvelopeIcon className="absolute size-7 left-3 top-1/2 -translate-y-1/2 text-neutral-400" />
+  ),
+  username: (
+    <UserIcon className="absolute size-7 left-3 top-1/2 -translate-y-1/2 text-neutral-400" />
+  ),
+  password: (
+    <KeyIcon className="absolute size-7 left-3 top-1/2 -translate-y-1/2 text-neutral-400" />
+  ),
+};
+
+export default function FormInput({
+  name,
+  errors,
+  ...rest
+}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="relative w-96">
       <input
-        type="email"
-        placeholder="Enter your email"
+        name={name}
+        {...rest}
         className="w-full pl-12 pr-4 py-2 rounded-full border-2 border-neutral-200"
       />
-      <EnvelopeIcon className="absolute size-7 left-3 top-1/2 -translate-y-1/2 text-neutral-400" />
+      {icon[name as IconName] || null}
     </div>
   );
 }
